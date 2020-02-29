843bd099ac6be839db6dcc2587a64efc4cb32326
山本周 <yamamotoshu@yamamotoamanenoMacBook-Air.local>
2020-02-29 12:49:35 +0900

    finish 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
 		@book = Book.new(book_params)
 		if @book.save
-		   redirect_to book_path(@book.id)
+		   redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
        @books = Book.all
        render 'index'
@@ -26,9 +26,9 @@ class BooksController < ApplicationController
   end
 
   def update
-		book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+		@book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
   end
 
   def destroy
@@ -39,6 +39,6 @@ class BooksController < ApplicationController
 
 	private
 	def book_params
-		params.require(:book).permit(:title, :body)
+		params.require(:book).permit(:title,:body)
 	end
 end
